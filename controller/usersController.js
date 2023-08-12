496f63352eb245e6d6796db8072f9995e6b2ba70
ANTi1302 <porkoe3443334@gmail.com>
2023-08-12 12:42:11 +0700

    test commit

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,39 +1,43 @@
-const salesforce = require('../config/loginSalesforce').getConnection();
-const returnResult = require('../utils/utilReturnData');
+const salesforce = require("../config/loginSalesforce").getConnection();
+const returnResult = require("../utils/utilReturnData");
+// controller for user
 class UsersController {
-    constructor(){
-        this.defaultFields = 'Id, Name, UserName__c, BirthDay__c, Email__c, Gender__c, Phone__c';
-    }
+  constructor() {
+    this.defaultFields =
+      "Id, Name, UserName__c, BirthDay__c, Email__c, Gender__c, Phone__c";
+  }
 
-    getAllUser = (req, res) => {
-        try {
-            salesforce.query(
-                `SELECT ${this.defaultFields} FROM Users__c`,
-            (error, result) => {
-                if (error) {
-                    returnResult.returnError(error, res);
-                }
-                returnResult.returnSuccess(result, res);
-            });
-        } catch (error) {
+  getAllUser = (req, res) => {
+    try {
+      salesforce.query(
+        `SELECT ${this.defaultFields} FROM Users__c`,
+        (error, result) => {
+          if (error) {
             returnResult.returnError(error, res);
+          }
+          returnResult.returnSuccess(result, res);
         }
+      );
+    } catch (error) {
+      returnResult.returnError(error, res);
     }
+  };
 
-    getUserbyId = (req, res) => {
-        const id = req.params.id;
-        try {
-            salesforce.query(
-                `SELECT ${this.defaultFields} FROM Users__c WHERE Id = '${id}'`,
-            (error, result) => {
-                if (error) {
-                    returnResult.returnError(error, res);
-                }
-                returnResult.returnSuccess(result, res);
-            });
-        } catch (error) {
+  getUserbyId = (req, res) => {
+    const id = req.params.id;
+    try {
+      salesforce.query(
+        `SELECT ${this.defaultFields} FROM Users__c WHERE Id = '${id}'`,
+        (error, result) => {
+          if (error) {
             returnResult.returnError(error, res);
+          }
+          returnResult.returnSuccess(result, res);
         }
+      );
+    } catch (error) {
+      returnResult.returnError(error, res);
     }
+  };
 }
 module.exports = new UsersController();
