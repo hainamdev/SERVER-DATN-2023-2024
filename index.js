dcea509a37619a40d1fe0964ef288c45bf907741
hainamdev <81764159+hainamdev@users.noreply.github.com>
2023-08-14 23:31:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const http = require("http");
 const app = require("./app");
-// const cors = require('cors');
 const server = http.createServer(app);
 const ultilLogMessage = require('./utils/ultilLogMessage');
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || 8080;
-
-// const allowedOrigins = ['http://localhost:3000'];
-
-// Config CORS
-// app.use(cors({
-//   origin: allowedOrigins,
-//   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-//   allowedHeaders: '*',
-//   exposedHeaders: '*',
-//   credentials: true,
-//   maxAge: 86400,
-//   optionsSuccessStatus: 204,
-// }));
-
 // server listening 
 server.listen(port, () => {
   ultilLogMessage.consoleLogBoxMessage("Server run on port: " + port);
